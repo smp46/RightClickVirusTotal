f9cce8b02894466ad2205b2bf3274df127e28870
Samuel <smp@smp-prestige>
2024-04-09 12:46:56 +1000

    Implemented CLI, fully functional with colour support

--- RightClickVirusTotal_CLI_Universal.py
+++ RightClickVirusTotal_CLI_Universal.py
@@ -7,6 +7,7 @@ from requests import post, get
 from time import sleep
 from threading import Thread
 import sys
+from colorama import Fore, Back, Style
 
 # Obtain the API key and file path from program arguments
 if len(sys.argv) < 3:
@@ -24,13 +25,15 @@ headers = {
     'x-apikey': VIRUSTOTALAPIKEY
 }
 
-def check_file(file_path, text_widget):
+def check_file():
     # Open the file in binary mode
     with open(file_path, 'rb') as file:
+        # Get the file name
+        file_name = basename(file_path)
+        print(f'\nChecking File: {file_name}')
         
         # Indicate that the file is being uploaded
-        text_widget.insert(tk.END, '[    ] File Uploading...', 'success')
-        text_widget.see(tk.END)
+        print(Fore.GREEN + '[ ] File Uploading...')
         
         # VirusTotal API requires a different request for files > 32MB
         file_size = getsize(file_path)
@@ -42,19 +45,14 @@ def check_file(file_path, text_widget):
             if response.status_code == 200:
                 globals()["url"] = response.json()['data']
             else:
-                text_widget.insert(tk.END, f'Error fetching upload URL: {response.status_code}\n', 'error')
-                text_widget.see(tk.END)
+                print(f'Error fetching upload URL: {response.status_code}\n')
                 return
 
         # Define the multipart-encoded data
         data = {'file': file}
 
         # Send the file to VirusTotal for scanning
-        response = post(url, headers=headers, files=data)
-
-        # Delete the uploading message
-        text_widget.delete('1.0', tk.END)
-        
+        response = post(url, headers=headers, files=data)        
 
         # Check if the request was successful
         if response.status_code == 200:
@@ -65,16 +63,12 @@ def check_file(file_path, text_widget):
             analysis_id = json_response['data']['id']
 
             # Update the text widget
-            text_widget.insert(tk.END, '[✓] File Uploaded\n', 'success')
-            text_widget.see(tk.END)
+            print("\033[A                             \033[A")
+            print('[✓] File Uploaded\n')
 
             # Get the results of the analysis
             analysis_url = f'https://www.virustotal.com/api/v3/analyses/{analysis_id}'
             analysis_response = get(analysis_url, headers=headers)
-
-            # Update the text widget
-            text_widget.insert(tk.END, '[    ] File Analysing', 'success')
-            text_widget.see(tk.END)
 
             # Initialize counter for loading dots
             loading_dots = 0
@@ -83,15 +77,12 @@ def check_file(file_path, text_widget):
             # Poll the analysis endpoint until the analysis is complete
             analysis_complete = False
             while not analysis_complete:
-                sleep(1)
-
                 # Loading animation
-                text_widget.delete('1.0', tk.END)
-                text_widget.insert(tk.END, '[✓] File Uploaded\n', 'success')
-                text_widget.insert(tk.END, '[    ] File Analysing', 'success')
-                text_widget.insert(tk.END, '.' * loading_dots, 'success')
-                text_widget.see(tk.END)
+                print("\033[A                             \033[A")
+                print(f'[ ] File Analysing' + '.' * loading_dots)
                 loading_dots = (loading_dots + 1) % 4
+
+                sleep(1)
 
                 # Perform the API request every 15 seconds
                 api_counter += 1
@@ -103,76 +94,34 @@ def check_file(file_path, text_widget):
                             analysis_complete = True
 
             # Redraw as complete
-            text_widget.delete('1.0', tk.END)
-            text_widget.insert(tk.END, '[✓] File Uploaded\n', 'success')
-            text_widget.insert(tk.END, '[✓] Analysis Complete\n\n', 'success')
-            text_widget.see(tk.END)
+            print("\033[A                             \033[A")
+            print('[✓] Analysis Complete\n')
+            print(Style.RESET_ALL, end ="")
 
             # Check if the request was successful
             if analysis_response.status_code == 200:
                 # Get the JSON response
                 analysis_json_response = analysis_response.json()
 
-                # Get the file name
-                file_name = basename(file_path)
-
                 # Get the results of the analysis
                 stats = analysis_json_response['data']['attributes']['stats']
 
                 # Update the text widget
-                text_widget.insert(tk.END, f'File: {file_name}\n', 'file')
-                text_widget.insert(tk.END, f'Harmless: {stats["harmless"]}\n', 'harmless')
-                text_widget.insert(tk.END, f'Malicious: {stats["malicious"]}\n', 'malicious')
-                text_widget.insert(tk.END, f'Suspicious: {stats["suspicious"]}\n', 'suspicious')
-                text_widget.insert(tk.END, f'Undetected: {stats["undetected"]}\n', 'undetected')
-                text_widget.see(tk.END)
+                print(Fore.BLUE + f'File: {file_name}')
+                print(Style.RESET_ALL, end =" ")
+                print(Fore.GREEN + f'Harmless: {stats["harmless"]}')
+                print(Style.RESET_ALL, end =" ")
+                print(Fore.RED + f'Malicious: {stats["malicious"]}')
+                print(Style.RESET_ALL, end =" ")
+                print(Fore.YELLOW + f'Suspicious: {stats["suspicious"]}')
+                print(Style.RESET_ALL, end =" ")
+                print(Fore.WHITE, end ="")
+                print(Style.DIM + f'Undetected: {stats["undetected"]}')
+                print(Style.RESET_ALL, end ="")
             else:
-                text_widget.insert(tk.END, f'Error: {analysis_response.status_code}\n', 'error')
-                text_widget.see(tk.END)
+                print(f'Error: {analysis_response.status_code}\n')
         else:
-            text_widget.insert(tk.END, f'Error: {response.status_code}\n', 'error')
-            text_widget.see(tk.END)
-
-def create_window():
-    # Create a new ThemedTk window
-    window = ThemedTk(theme="arc")
-
-    # Set the size of the window
-    window.geometry("300x300")
-    
-    # # Attempt to help smooth text in 
-    # windll.shcore.SetProcessDpiAwareness(1)
-
-    # Set the title of the window
-    window.title("VirusTotal Scanner")
-
-    # Get the path to the directory where the executable is running
-    base_path = getattr(sys, '_MEIPASS', dirname(abspath(__file__)))
-
-    # Add an exit button at the bottom of the window
-    exit_button = tk.Button(window, text="Exit", command=window.destroy, padx=0, pady=10, font=("Helvetica", 12),
-                            bg='#2c2c2c', fg='#f5f6fa', activebackground='#3c3c3c', activeforeground='#f5f6fa',
-                            bd=0, highlightthickness=0)
-    exit_button.pack(side='bottom', fill='x', expand=True)
-
-    # Create a new text widget with padding
-    text_widget = tk.Text(window, padx=10, pady=10, wrap="word", font=("Helvetica", 12))
-    text_widget.pack(fill=tk.BOTH, expand=True)
-    
-    # Define text colors
-    text_widget.tag_configure('success', foreground='green')
-    text_widget.tag_configure('error', foreground='red')
-    text_widget.tag_configure('file', foreground='blue')
-    text_widget.tag_configure('harmless', foreground='green')
-    text_widget.tag_configure('malicious', foreground='red')
-    text_widget.tag_configure('suspicious', foreground='orange')
-    text_widget.tag_configure('undetected', foreground='grey')
-
-    # Start the file check in a new thread to avoid freezing the GUI
-    Thread(target=check_file, args=(file_path, text_widget), daemon=True).start()
-
-    # Start the Tkinter event loop
-    window.mainloop()
+            print(f'Error: {response.status_code}\n')
 
 if __name__ == "__main__":
-    create_window()
+    check_file()
