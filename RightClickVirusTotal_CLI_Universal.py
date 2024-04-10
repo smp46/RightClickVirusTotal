facf0f85d2f6607d773767bfbbdc0f6dadf624af
smp46 <27676057+smp46@users.noreply.github.com>
2024-04-10 11:40:51 +1000

    Major rework of codebase. Now utilising the vt-py library for better reliability. All versions migrated and now stable.

--- RightClickVirusTotal_CLI_Universal.py
+++ RightClickVirusTotal_CLI_Universal.py
@@ -3,120 +3,123 @@ from requests import post, get
 from time import sleep
 import sys
 from colorama import Fore, Style
+import vt
+from hashlib import md5
 
-# Obtain the API key and file path from program arguments
-if len(sys.argv) < 3:
-    print("Usage: <script> <VirusTotalAPIKey> <FilePath>")
-    sys.exit()
-
-VIRUSTOTALAPIKEY = sys.argv[1].strip()
-file_path = sys.argv[2].strip()
-
-# Define the API endpoint
-url = 'https://www.virustotal.com/api/v3/files'
-
-# Define the headers for the API request
-headers = {
-    'x-apikey': VIRUSTOTALAPIKEY
-}
-
-def check_file():
-    # Open the file in binary mode
-    with open(file_path, 'rb') as file:
-        # Get the file name
-        file_name = basename(file_path)
+def interface(print_stage, file_name=None, loading_dots=None, stats=None):
+    if (print_stage == "title"):
+        print(r"  ___ _      _   _    ___ _ _    _  __   ___             _____    _        _ ")
+        print(r" | _ (_)__ _| |_| |_ / __| (_)__| |_\ \ / (_)_ _ _  _ __|_   ____| |_ __ _| |")
+        print(r" |   | / _` | ' |  _| (__| | / _| / /\ V /| | '_| || (_-< | |/ _ |  _/ _` | |")
+        print(r" |_|_|_\__, |_||_\__|\___|_|_\__|_\_\ \_/ |_|_|  \_,_/__/ |_|\___/\__\__,_|_|")
+        print(r"       |___/                                                                 ")
+    elif (print_stage == "uploading"):
         print(f'\nChecking File: {file_name}')
+        print(Fore.GREEN + "[ ] File Uploading...")
+    elif (print_stage == "uploaded"):
+        print("\033[A                             \033[A")
+        print('[✓] File Uploaded\n')
+    elif (print_stage == "analysing"):
+        print("\033[A                             \033[A")
+        print(f'[ ] File Analysing' + '.' * loading_dots)
+    elif (print_stage == "analysed"):
+        print("\033[A                             \033[A")
+        print('[✓] Analysis Complete\n')
+        print(Style.RESET_ALL, end ="")
+    elif (print_stage == "results"):
+        print(Fore.BLUE + f'File: {file_name}')
+        print(Style.RESET_ALL, end =" ")
+        print(Fore.GREEN + f'Harmless: {stats["harmless"]}')
+        print(Style.RESET_ALL, end =" ")
+        print(Fore.RED + f'Malicious: {stats["malicious"]}')
+        print(Style.RESET_ALL, end =" ")
+        print(Fore.YELLOW + f'Suspicious: {stats["suspicious"]}')
+        print(Style.RESET_ALL, end =" ")
+        print(Fore.WHITE, end ="")
+        print(Style.DIM + f'Undetected: {stats["undetected"]}')
+        print(Style.RESET_ALL, end ="")
         
-        # Indicate that the file is being uploaded
-        print(Fore.GREEN + '[ ] File Uploading...')
-        
-        # VirusTotal API requires a different request for files > 32MB
-        file_size = getsize(file_path)
-        max_size = 32 * 1024 * 1024 
+def hash_file(file_path):
+    # Open the file in binary mode and hash it
+    with open(file_path, 'rb') as file_to_check:
+        data = file_to_check.read() 
+        md5_hash = md5(data).hexdigest()
+    return md5_hash
 
-        if file_size > max_size:
-            # For large files, first get the upload URL
-            response = get("https://www.virustotal.com/api/v3/files/upload_url", headers=headers)
-            if response.status_code == 200:
-                globals()["url"] = response.json()['data']
-            else:
-                print(f'Error fetching upload URL: {response.status_code}\n')
-                return
+def scan_file(client, file_path):
+    loading_dots = 0
+    api_counter = 28
+    
+    with open(file_path, "rb") as f:
+        analysis = client.scan_file(f)
 
-        # Define the multipart-encoded data
-        data = {'file': file}
+    # Poll the analysis endpoint until the analysis is complete
+    analysis_complete = False
+    while not analysis_complete:
+        # Loading animation
+        interface("analysing", loading_dots=loading_dots)
+        loading_dots = (loading_dots + 1) % 4
 
-        # Send the file to VirusTotal for scanning
-        response = post(url, headers=headers, files=data)        
+        # Sleep for 1 second
+        sleep(1)
 
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Get the JSON response
-            json_response = response.json()
+        # Perform the API request every 15 seconds
+        if api_counter % 15 == 0:
+            analysis = client.get_object("/analyses/{}", analysis.id)
+            if analysis.status == "completed":
+                analysis_complete = True
 
-            # Get the ID of the analysis
-            analysis_id = json_response['data']['id']
+        api_counter += 1
+    return  
 
-            # Update the text widget
-            print("\033[A                             \033[A")
-            print('[✓] File Uploaded\n')
+def main():
+    # Obtain the API key and file path from program arguments
+    VIRUSTOTALAPIKEY = sys.argv[1].strip()
+    file_path = sys.argv[2].strip()
+    
+    # Get file name
+    file_name = basename(file_path)
+    
+    # Print the title
+    interface(print_stage="title")
+    # Print the uploading message
+    interface("uploading", file_name=file_name)
+    
+    # Initialise vt-api client
+    client = vt.Client(VIRUSTOTALAPIKEY)
+    md5_hash = hash_file(file_path)
 
-            # Get the results of the analysis
-            analysis_url = f'https://www.virustotal.com/api/v3/analyses/{analysis_id}'
-            analysis_response = get(analysis_url, headers=headers)
+    # Determine if file has already been scanned, if not try scanning it
+    try:
+        file_object = client.get_object("/files/" + md5_hash)
+        interface("uploaded")
+    except:
+        scan_file(client, file_path)
+        try:
+            file_object = client.get_object("/files/" + md5_hash)
+            interface("uploaded")
+        except:
+            interface("error")
+            return
+    
+    # Print the analysis complete message
+    interface("analysed")
+    # Print the results
+    interface("results", file_name=file_name, stats=file_object.last_analysis_stats)
 
-            # Initialize counter for loading dots
-            loading_dots = 0
-            api_counter = 0
+    # Close the client
+    client.close()
 
-            # Poll the analysis endpoint until the analysis is complete
-            analysis_complete = False
-            while not analysis_complete:
-                # Loading animation
-                print("\033[A                             \033[A")
-                print(f'[ ] File Analysing' + '.' * loading_dots)
-                loading_dots = (loading_dots + 1) % 4
+    # Ensure all terminal colouring is reset
+    print(Style.RESET_ALL, end ="")
 
-                sleep(1)
-
-                # Perform the API request every 15 seconds
-                api_counter += 1
-                if api_counter % 15 == 0:
-                    analysis_response = get(analysis_url, headers=headers)
-                    if analysis_response.status_code == 200:
-                        analysis_status = analysis_response.json()['data']['attributes']['status']
-                        if analysis_status == 'completed':
-                            analysis_complete = True
-
-            # Redraw as complete
-            print("\033[A                             \033[A")
-            print('[✓] Analysis Complete\n')
-            print(Style.RESET_ALL, end ="")
-
-            # Check if the request was successful
-            if analysis_response.status_code == 200:
-                # Get the JSON response
-                analysis_json_response = analysis_response.json()
-
-                # Get the results of the analysis
-                stats = analysis_json_response['data']['attributes']['stats']
-
-                # Update the text widget
-                print(Fore.BLUE + f'File: {file_name}')
-                print(Style.RESET_ALL, end =" ")
-                print(Fore.GREEN + f'Harmless: {stats["harmless"]}')
-                print(Style.RESET_ALL, end =" ")
-                print(Fore.RED + f'Malicious: {stats["malicious"]}')
-                print(Style.RESET_ALL, end =" ")
-                print(Fore.YELLOW + f'Suspicious: {stats["suspicious"]}')
-                print(Style.RESET_ALL, end =" ")
-                print(Fore.WHITE, end ="")
-                print(Style.DIM + f'Undetected: {stats["undetected"]}')
-                print(Style.RESET_ALL, end ="")
-            else:
-                print(f'Error: {analysis_response.status_code}\n')
-        else:
-            print(f'Error: {response.status_code}\n')
+    # Exit the program
+    sys.exit()          
 
 if __name__ == "__main__":
-    check_file()
+    # Ensure the correct number of arguments are provided
+    if len(sys.argv) < 3:
+        print("Usage: <script> <VirusTotalAPIKey> <FilePath>")
+        sys.exit()
+
+    main()
